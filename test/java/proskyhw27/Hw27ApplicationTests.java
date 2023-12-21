3e75667165563a45c12e64542bbd9fdf4deda48b
Танин <mazayztn@yandex.ru>
2023-12-21 15:00:44 +0300

    initial commit

---
+++ test/java/proskyhw27/Hw27ApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyhw27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
